cbfe9f9959214bcdeb2ddefcddaa66c22d2d97f1
igor-lourenco <igor.lourencosantos@gmail.com>
2020-07-31 14:27:29 -0300

    Design DepartamentoForma

--- src/gui/DepartamentoListaController.java
+++ src/gui/DepartamentoListaController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alertas;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import modelo.entidades.Departamento;
 import modelo.servicos.DepartamentoServico;
@@ -34,8 +43,9 @@ public class DepartamentoListaController implements Initializable {
 	
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("On buttom Action new");
+	public void onBtNovoAction(ActionEvent event) {
+		Stage parentStage = Utils.palcoAtual(event); 
+		criarFormaDialogo("/gui/DepartamentoForma.fxml", parentStage);
 	}
 
 	public void setDepartamentoServico(DepartamentoServico servico) {
@@ -65,4 +75,21 @@ public class DepartamentoListaController implements Initializable {
 		tableViewDepartamento.setItems(obsLista);
 	}
 
+	private void criarFormaDialogo(String nomeAbsoluto, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Digite os dados do departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		} catch(IOException e) {
+			Alertas.showAlert("IO Exception", "Erro ao carregar página", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
